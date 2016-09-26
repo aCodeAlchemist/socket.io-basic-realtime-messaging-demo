6e628755babc6842b244f68daa721851d318560d
shreejibawa <shah.shreeji248@gmail.com>
2016-09-26 14:28:12 +0530

    initial

---
+++ public/lib/js/main.js
@@ -0,0 +1,73 @@
+/**
+ *  Module
+ *
+ * Description
+ */
+var app = angular.module('ngEditor', ['textAngular', 'ngSanitize', 'toastr']);
+
+app.controller('textEditorCtrl', ['$scope', '$sce', '$timeout', 'toastr', function($scope, $sce, $timeout, toastr) {
+    // var socket = io.connect("http://10.10.5.149:3000");
+    var socket = io.connect("http://messaging.labs.webmpires.net");
+    // var socket = io.connect("http://192.168.1.100:3000");
+    var audio = new Audio('sounds/alert.mp3');
+    
+    $scope.messages = [];
+    $scope.data = [];
+
+    socket.on("changedValue", function(data) {
+        $scope.$evalAsync(function(scope) {
+            audio.play();
+            scope.messages.unshift(data);
+        });
+    });
+
+    socket.on("userAdded", function(data) {
+        $scope.$evalAsync(function(scope) {
+            if (data.joined !== $scope.userName) {
+                toastr.success(data.joined + " has joined.");
+            }
+            scope.allUsers = data.allUsers;
+        });
+    });
+
+    socket.on("userLeft", function(data) {
+        angular.forEach($scope.allUsers, function(user, idx) {
+            $scope.discIdx = user.name == data.left ? idx : '';
+        });
+        console.log($scope.discIdx);
+        if (data.left) {
+            toastr.warning(data.left + " has left.");
+        }
+        $scope.$evalAsync(function(scope) {
+            $timeout(function() {
+                scope.allUsers = data.allUsers;
+            }, 1000);
+        });
+        console.log(data);
+    });
+
+    socket.on("addExistingContent", function(data) {
+        $scope.$evalAsync(function(scope) {
+            scope.allUsers = data.allUsers;
+        });
+    });
+
+    $scope.addMe = function() {
+        if (!$scope.userName) {
+            return;
+        }
+        socket.emit("addUser", {
+            name: $scope.userName
+        });
+        $scope.isUserAdded = true;
+    };
+
+    $scope.send = function(text) {
+        $scope.messages.unshift({ text: $scope.data.message, sent: true, user: $scope.userName });
+        socket.emit("addedMessage", {
+            text: $scope.data.message,
+            user: $scope.userName
+        });
+        $scope.data.message = '';
+    }
+}]);
---
+++ routes/socket.js
@@ -0,0 +1,29 @@
+module.exports = function(app, io) {
+    var users = [],
+        existingContent;
+    io.on('connection', function(socket) {
+        console.log("user connected...");
+        socket.on("addUser", function(data) {
+            users.push(data);
+            socket.name = data.name;
+            io.emit('userAdded', { joined: data.name, allUsers: users });
+        });
+
+        // socket.emit("addExistingContent", { data: existingContent, allUsers: users });
+
+        socket.on("addedMessage", function(data) {
+            data.sent = false;
+            socket.broadcast.emit('changedValue', data);
+        });
+
+        socket.on('disconnect', function() {
+            for (var i = 0; i < users.length; i++) {
+                if (users[i].name == socket.name) {
+                    users.splice(i, 1);
+                }
+            }
+            console.log(users);
+            socket.broadcast.emit('userLeft', { left: socket.name, allUsers: users });
+        });
+    });
+};
